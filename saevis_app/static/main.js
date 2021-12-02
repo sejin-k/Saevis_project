5943b802c732a0754013b97d90a0335830f78b12
sejin-k <saejin7649@gmail.com>
2021-12-02 21:23:14 +0900

    [Update] main화면 : navbar 고정

--- saevis_app/static/main.js
+++ saevis_app/static/main.js
@@ -2,7 +2,14 @@ const menuBtn = document.querySelector('.navbar__menuBtn');
 const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
 
+const testBtn = document.querySelector('.testBtn');
+const test = document.querySelector('.services');
+
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
+})
+
+testBtn.addEventListener('click', () => {
+    test.classList.toggle('active');
 })
